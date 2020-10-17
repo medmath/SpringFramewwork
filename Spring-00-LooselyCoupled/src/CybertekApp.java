fa1082b432b3aa590ce3dbb0847e7f915ed54eef
medmath <medmathandit@gmail.com>
2020-10-17 13:43:47 -0400

    Spring Container added

--- Spring-00-LooselyCoupled/src/CybertekApp.java
+++ Spring-00-LooselyCoupled/src/CybertekApp.java
@@ -1,12 +1,16 @@
 import implementation.Mentor;
 import service.FullTimeMentor;
 import service.MentorAccount;
+import service.PartTimeMentor;
 
 public class CybertekApp {
 
     public static void main(String[] args) {
-        FullTimeMentor fullTime = new FullTimeMentor();
-        MentorAccount mentor1 = new MentorAccount(fullTime);
+//        FullTimeMentor fullTime = new FullTimeMentor();
+
+        PartTimeMentor partTime = new PartTimeMentor();
+
+        MentorAccount mentor1 = new MentorAccount(partTime);
         mentor1.manageAccount();
     }
 }
