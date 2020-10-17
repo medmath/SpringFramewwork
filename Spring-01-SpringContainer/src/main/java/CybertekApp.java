9ff0c82da4535e0b929373c6aee74c388293527c
medmath <medmathandit@gmail.com>
2020-10-17 14:48:12 -0400

    XML config completed

--- Spring-01-SpringContainer/src/main/java/CybertekApp.java
+++ Spring-01-SpringContainer/src/main/java/CybertekApp.java
@@ -1,11 +1,33 @@
+import interfaces.Mentor;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import services.FullTimeMentor;
 
 public class CybertekApp {
 
     public static void main(String[] args) {
 // we are gonna use XML Config... we addressed the xml file
-        BeanFactory container = new ClassPathXmlApplicationContext("config.xml");//
+        //BeanFactory container = new ClassPathXmlApplicationContext("config.xml");//
+
+
+        ApplicationContext container =new ClassPathXmlApplicationContext("config.xml");
+
+
+        Mentor mentor = (Mentor)container.getBean("fullTimeMentor");//new FullTimeMentor();// instead of using new
+// container.getBean(id from XML file), but it is complaining, returning an object, so we need to cast to change its reference type
+
+        //Second way
+        Mentor mentor2 =container.getBean("fullTimeMentor", Mentor.class);// it is a different way
+
+
+        mentor.createAccount();
+
+
+        Mentor mentor1 = (Mentor)container.getBean("partTimeMentor");
+        mentor1.createAccount();
+
+        mentor2.createAccount();
 
     }
 }
